aefa9f2cbf34013e4bd4832ac52a6c56ceb89a87
liang.guo <547044269@qq.com>
2023-02-03 09:23:12 +0800

    提交版本

---
+++ src/main/java/org/example/HelloController.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liang.guo
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world!";
+    }
+}
